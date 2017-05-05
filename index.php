d1a258e2a44da4865119a4b557d3c0b42647b451
Arya Wiranata <wiranata.arya.wiranata@gmail.com>
2017-05-05 13:35:41 +0700

    Tambahan front end index

--- index.php
+++ index.php
@@ -1,20 +1,66 @@
 <?php
-	session_start();
-	if(!isset($_SESSION['email'])){
-		header("Location: login.php");
-		die;
-	}
-
-	echo "Halaman ini muncul jika anda sudah berhasil login";
-	?><a href="logout.php">Keluar</a><?php
-?>
+  session_start();
+  if(!isset($_SESSION['email'])){
+    header("Location: login.php");
+    die;
+  }
+  ?>
 
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-	<title>Home</title>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
+  <title>COMING SOON</title>
+
+  <!-- CSS  -->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+  <link href="css/style_index.css" type="text/css" rel="stylesheet" media="screen,projection"/>
 </head>
 <body>
+  <nav class="light-blue lighten-1" role="navigation">
+    <div class="nav-wrapper container">
+      <ul class="right hide-on-med-and-down">
+        <li><a href="logout.php">Logout</a></li>
+      </ul>
 
-</body>
+      <ul id="nav-mobile" class="side-nav">
+        <li><a href="logout.php">Logout</a></li>
+      </ul>
+      <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
+    </div>
+  </nav>
+  <div class="section no-pad-bot" id="index-banner">
+    <div class="container">
+      <br><br>
+      <h1 class="header center orange-text">COMING SOON</h1>
+      <div class="row center">
+        <h5 class="header col s12 light">Our website is under construction. Thank you for your patience</h5>
+      </div>
+      
+      <br><br>
+
+    </div>
+  </div>
+
+
+  
+
+  <footer class="page-footer orange" style="position: relative;margin-top: 228px; /* negative value of footer height */;clear: both;">
+    
+    <div class="footer-copyright">
+      <div class="container">
+      Made by <span class="orange-text text-lighten-3">Kuliahku - MBD E</span> 
+      </div>
+    </div>
+  </footer>
+
+
+  <!--  Scripts-->
+  <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+  <script src="js/materialize.js"></script>
+  <script src="js/init.js"></script>
+
+  </body>
 </html>
